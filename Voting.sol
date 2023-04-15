765d7a421d7c536978c544426afea6967fa1dcd4
Thisara_Gunasekara <66357507+Thisara290@users.noreply.github.com>
2023-04-15 09:37:42 +0530

    withdraw deposit get address balance
    remix id solidity code withdraw deposit get address and balance

---
+++ Voting.sol
@@ -0,0 +1,31 @@
+
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+
+
+contract Voting  {
+
+
+
+function deposit() external payable {
+}
+
+  
+function withdraw(address payable _to, uint _amount) external {
+        _to.transfer(_amount);
+}
+
+  
+
+function getBalance() external view returns(uint){
+        return address(this).balance;
+} 
+
+function getAddress() external view returns(address) {
+        return address(this);
+}
+
+}
