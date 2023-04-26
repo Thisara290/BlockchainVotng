d99e9e8aa19651a70882d015c641d1d05906df56
Thisara_Gunasekara <66357507+Thisara290@users.noreply.github.com>
2023-04-26 19:44:56 +0530

    Vote
    voting functions remix

--- Voting.sol
+++ Voting.sol
@@ -27,7 +27,118 @@ contract Voting is ERC20,Ownable,ReentrancyGuard {
 
 
 
+address[] public candidateList;
+address[] public voterslist;
 
+mapping (address => uint8) public votesRecieved;
+mapping(address => bool) public hasVoted;
+mapping(address => bool) public hasParticipated;
+
+
+address public winner;
+uint public winnerVotes;
+
+enum VotingStatus { NotStarted, Running, Completed }
+VotingStatus public status;
+
+
+modifier onlyRunning() {
+    require(status == VotingStatus.Running, "Election is not active");
+    _;
+}
+
+function setStatus() public onlyOwner {
+    if (status != VotingStatus.Completed && status != VotingStatus.Running){
+        status = VotingStatus.Running;
+    }
+    else {
+        status = VotingStatus.Completed;
+    }
+}
+
+function resetVoting() external onlyOwner {
+    require(status == VotingStatus.Completed, "Voting must be completed before it can be reset.");
+    require(candidateList.length > 0, "There are no candidates registered to reset.");
+    
+    for (uint i = 0; i < candidateList.length; i++) {
+        delete votesRecieved[candidateList[i]];
+    }
+    delete candidateList;
+    delete voterslist;
+    delete winner;
+    winnerVotes = 0;
+    status = VotingStatus.NotStarted;
+}
+
+
+function registerCandidate(address _candidate) public onlyOwner {
+    require(!validateCandidate(_candidate), "Candidate is already registered");
+    candidateList.push(_candidate);
+}
+
+function vote(address _candidate) public onlyRunning {
+    require(validateCandidate(_candidate), "Not a valid Candidate");
+    require(!hasVoted[msg.sender], "You have already voted");
+    votesRecieved[_candidate] += 1;
+    hasVoted[msg.sender] = true;
+    hasParticipated[msg.sender] = true;
+    voterslist.push(msg.sender);
+    
+}
+
+function validateCandidate(address _candidate) view public returns(bool) {
+    for(uint i = 0; i < candidateList.length; i++){
+        if(candidateList[i] == _candidate){
+            return true;
+        }
+    }
+    return false;
+}
+
+function getVotesCount(address _candidate) public view onlyRunning returns(uint) {
+    require(validateCandidate(_candidate), "Not a valid Candidate");
+    return votesRecieved[_candidate];
+}
+
+function result() public onlyOwner {
+    require(status == VotingStatus.Completed, "voting is not completed");
+    for(uint i = 0; i < candidateList.length; i++){
+        if(votesRecieved[candidateList[i]] > winnerVotes){
+            winnerVotes = votesRecieved[candidateList[i]];
+            winner = candidateList[i];
+        }
+    }
+}
+
+function getWinner() public view returns (address) {
+    require(status == VotingStatus.Completed, "Voting is not completed yet");
+    require(winner != address(0), "Winner has not been determined yet");
+    
+    return winner;
+}
+function getWinnerVotes() public view returns (uint) {
+    require(status == VotingStatus.Completed, "Voting is not completed yet");
+    require(winner != address(0), "Winner has not been determined yet");
+    
+    return winnerVotes;
+}
+function getCandidateList() public view returns (address[] memory) {
+    return candidateList;
+}
+
+function getVotersList() public view returns (address[] memory) { //get the list of people participated for election to vote
+    require(status == VotingStatus.Completed, "Voting is not completed yet");
+    return voterslist;
+}
+function getStatusText() public view returns (string memory) {
+    if (status == VotingStatus.NotStarted) {
+        return "NotStarted";
+    } else if (status == VotingStatus.Running) {
+        return "Running";
+    } else {
+        return "Completed";
+    }
+}
 
 function deposit() external payable {
 }
